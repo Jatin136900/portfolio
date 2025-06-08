d9f4552db2e042a268d456fbd0c4be4e6b122edd
Jatin <jatinverma2428@gmail.com>
2025-06-08 17:19:15 +0530

    first

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
- const loadMoreBtn = document.querySelector(".load-more");
-    const hiddenSections = document.querySelectorAll(".cards");
+const loadMoreBtn = document.querySelector(".load-more");
+const hiddenSections = document.querySelectorAll(".cards");
 
-    loadMoreBtn.addEventListener("click", () => {
-      hiddenSections.forEach(section => section.style.display = "block");
-      loadMoreBtn.style.display = "none";
-    });
-
-
+loadMoreBtn.addEventListener("click", () => {
+  hiddenSections.forEach(section => section.style.display = "block");
+  loadMoreBtn.style.display = "none";
+});
 
 
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+   
+    const name = this.name.value;
+    const email = this.email.value;
+    const message = this.message.value;
+
+   
+    alert(`Thanks ${name}! Your message has been received.`);
+
+     
+    this.reset();
+});
